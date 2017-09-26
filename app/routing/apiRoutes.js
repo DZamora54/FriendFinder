e82fa8cc566f82c4c5e67c01b3abab76c40248ee
DZamora54 <dantedomenic@hotmail.com>
2017-09-26 18:29:03 -0400

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -30,13 +30,4 @@ module.exports = function(app) {
     friends.push(req.body);
     res.json(true);
   });
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-//   app.post("/api/clear", function() {
-//     // Empty out the arrays of data
-//     tableData = [];
-//     waitListData = [];
-//     console.log(tableData);
-//   });
  };
